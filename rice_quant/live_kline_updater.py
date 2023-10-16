4fd3e6edbc3d9123f948c306365c7e6b0bdc9c90
Suying Zhou <zhou.sy@yz-fund.com.cn>
2023-10-16 17:50:23 +0800

    optimize the efficiency of the code

--- rice_quant/live_kline_updater.py
+++ rice_quant/live_kline_updater.py
@@ -13,28 +13,21 @@ from util.utils import send_email, SendEmailInfo
 from choice.kc_stock_number import get_kc_stock_num
 
 
-def gen_ricequant_virtual_kline():
-    date = time.strftime('%Y%m%d')
+def gen_ricequant_virtual_kline(stock_list,date):
     current_min = int(time.strftime('%H%M'))
-    print(f'Generate RiceQuant virtual kline at {datetime.datetime.now()}')
-    rq_vk_df = gen_rq_vk_df(date)
+    print(f'Generating RiceQuant virtual kline at {datetime.datetime.now()}')
+    rq_vk_df = gen_rq_vk_df(stock_list)
     rq_vk_df.to_pickle(
         rf"\\192.168.1.116\kline\virtual\virtual_kline2_{date}_{current_min}.pkl")  # for youwei
     rq_vk_df.to_csv(
         rf"\\192.168.1.116\kline\virtual_csv\virtual_kline2_{date}_{current_min}.csv")  # for shaohu
     print(f'Downloaded at {datetime.datetime.now()}')
-    kc_stock_num = get_kc_stock_num()
-    check_rq_virtual_kline(rq_vk_df, date, kc_stock_num)
+
+    check_rq_virtual_kline(rq_vk_df)
     print(datetime.datetime.now())
-    time.sleep(60)
 
-def gen_rq_vk_df(date):
-    rq.init()
-    cn_stks_df = rq.all_instruments(type='CS', market='cn', date=date)
-    kc_stks_df = cn_stks_df.query("board_type == 'KSH'").set_index('order_book_id')
-
-    # 获取快照数据
-    snapshot = rq.current_snapshot(kc_stks_df.index)
+def gen_rq_vk_df(stock_list):
+    snapshot = rq.current_snapshot(stock_list)
     vk_data = [[x.order_book_id, x.open, x.high, x.low, x.last,
                 x.volume, x.total_turnover, x.datetime, x.prev_close] for x in snapshot]
     vk_df = pd.DataFrame(vk_data, columns=[
@@ -47,31 +40,46 @@ def gen_rq_vk_df(date):
     vk_df.index.name = 'symbol'
     return vk_df
 
+def gen_stock_list(date):
+    rq.init()
+    cn_stks_df = rq.all_instruments(type='CS', market='cn', date=date)
+    kc_stks_df = cn_stks_df.query("board_type == 'KSH'").set_index('order_book_id')
 
-def check_rq_virtual_kline(rq_vk_df, date, kc_stock_num):
+    # 获取快照数据
+
+    return kc_stks_df.index
+
+
+
+
+
+def check_rq_virtual_kline(rq_vk_df):
+    kc_stock_num = get_kc_stock_num()
     print('Check RiceQuant virtual kline...')
     error_dict = {
-        'stock count': rq_vk_df.shape[0],
-        'stock count from choice': kc_stock_num,
         'NaN stock': rq_vk_df[rq_vk_df.isnull().any(axis=1)].index.tolist(),
         'duplicate stock': rq_vk_df[rq_vk_df.index.duplicated()].index.tolist(),
         'zero': rq_vk_df[(rq_vk_df == 0).any(axis=1)].index.tolist(),
         'negative': rq_vk_df[(rq_vk_df < 0).any(axis=1)].index.tolist(),
-        'Extreme high low difference': rq_vk_df[
-            (rq_vk_df['high'] - rq_vk_df['low']) / rq_vk_df['low'] > 0.21].index.tolist(),
-        'Extreme intraday return': rq_vk_df[
-            (abs(rq_vk_df['high'] - rq_vk_df['preclose']) / rq_vk_df['preclose']) > 0.21].index.tolist(),
         'Open & now out of High & low': rq_vk_df[
             (rq_vk_df['open'] > rq_vk_df['high']) | (rq_vk_df['open'] < rq_vk_df['low']) | (
                     rq_vk_df['now'] > rq_vk_df['high']) | (rq_vk_df['now'] < rq_vk_df['low'])].index.tolist(),
 
     }
-    if all(item == [] for item in list(error_dict.values())[2:]) & (
-            error_dict['stock count'] == error_dict['stock count from choice']):
+    info_dict = {
+        'stock count': rq_vk_df.shape[0],
+        'stock count from choice': kc_stock_num,
+        'Extreme high low difference': rq_vk_df[
+            (rq_vk_df['high'] - rq_vk_df['low']) / rq_vk_df['low'] > 0.50].index.tolist(),
+        'Extreme intraday return': rq_vk_df[
+            (abs(rq_vk_df['high'] - rq_vk_df['preclose']) / rq_vk_df['preclose']) > 0.21].index.tolist(),
+    }
+    if all(item == [] for item in list(error_dict.values())) & (
+            info_dict['stock count'] == info_dict['stock count from choice']):
         print('No error found in RiceQuant virtual kline')
     else:
         print('Error found in RiceQuant virtual kline, sending email...')
-        notify_with_email(error_dict)
+        notify_with_email({**error_dict,**info_dict})
 
 
 def notify_with_email(error_dict):
@@ -94,11 +102,10 @@ def notify_with_email(error_dict):
     <p>Open & now out of High & low: {error_dict['Open & now out of High & low']}</p>
     
     """
-    send_email(subject=subject, content=text, receiver=SendEmailInfo.department['research'])
+    send_email(subject=subject, content=text, receiver=SendEmailInfo.department['research'][0])
 
 
 
 if __name__ == '__main__':
-    while True:
-        gen_ricequant_virtual_kline()
-        time.sleep(60)
+    gen_ricequant_virtual_kline(gen_stock_list('20231016'),'20231016')
+
