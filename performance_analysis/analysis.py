9564125da4432a6d2f9b4ab447be4c3e0523f170
Suying Zhou <zhou.sy@yz-fund.com.cn>
2023-10-20 13:17:12 +0800

    输出当日踏浪一号收益率在科创板中的表现

---
+++ performance_analysis/analysis.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/10/18 15:59
+# @Author  : Suying
+# @Site    : 
+# @File    : analysis.py
+import time
+import pandas as pd
+import numpy as np
+from performance_analysis.data_acquisition import get_talang1_ret, get_kc_stock_pct, get_kc50_stock_list, get_kc50_ret
+from util.send_email import Mail
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.io import write_html
+
+
+def daily_performance_eval(date=None):
+    date = time.strftime('%Y-%m-%d') if date is None else pd.to_datetime(date).strftime("%Y-%m-%d")
+    data_dict = get_data(date)
+    plot_hist_performance(data_dict['kc stock'], data_dict['talang1'], data_dict['kc50'], '科创板股票', date)
+    plot_hist_performance(data_dict['kc50 stock'], data_dict['talang1'], data_dict['kc50'], '科创50成分股', date)
+    statistics_df = gen_statistics_table(data_dict['kc stock'], data_dict['kc50 stock'])
+    notify_with_email(statistics_df, data_dict['talang1'], data_dict['kc50'], date)
+
+
+def notify_with_email(df_html, talang1_ret, kc50_ret, date=None):
+    date = time.strftime('%Y-%m-%d') if date is None else pd.to_datetime(date).strftime("%Y-%m-%d")
+
+    subject = f'[Strategy Review] {date}'
+    content = f"""
+    <table width="800" border="0" cellspacing="0" cellpadding="4">
+    <tr>
+    <td bgcolor="#CECFAD" height="30" style="font-size:21px"><b>踏浪1号在科创板表现一览</b></td>
+    </tr>
+    <td bgcolor="#EFEBDE" height="100" style="font-size:13px">
+    """
+    content += f"""
+    <p>科创板和科创50成分股表现统计：</p>
+    <center>{df_html}</center>
+    <p>踏浪1号当日收益率：{format_number(talang1_ret)}</p>
+    <p>科创50当日收益率：{format_number(kc50_ret)}</p>
+    """
+    Mail(receivers=['zhou.sy@yz-fund.com.cn', 'wu.yw@yz-fund.com.cn']).send(
+        subject,
+        content,
+        attachs=[],
+        pics=[f'./data/科创板股票涨跌幅分布_{date}.png', f'./data/科创50成分股涨跌幅分布_{date}.png'],
+        pic_disp=['科创板涨跌幅分布', '科创50涨跌幅分布']
+    )
+
+
+def gen_statistics_table(kc_stock_ret, kc50_stock_ret):
+    kc_stock_statistics = gen_statistics(kc_stock_ret, '科创板股票')
+    kc50_stock_statistics = gen_statistics(kc50_stock_ret, '科创50成分股')
+    statistics_df = pd.concat([kc_stock_statistics, kc50_stock_statistics], axis=1)
+
+    df_html = statistics_df.to_html()
+
+    return df_html
+
+
+def gen_statistics(kc_stock_ret, stock_name):
+    statistics_s = pd.Series(data={
+        '上涨数': len(kc_stock_ret[kc_stock_ret > 0]),
+        '下跌数': len(kc_stock_ret[kc_stock_ret < 0]),
+        '持平数': len(kc_stock_ret[kc_stock_ret == 0]),
+        '上涨比例': len(kc_stock_ret[kc_stock_ret > 0]) / len(kc_stock_ret),
+        '均值': np.mean(kc_stock_ret),
+        '中位数': np.median(kc_stock_ret),
+        '标准差': np.std(kc_stock_ret),
+        '最大值': np.max(kc_stock_ret),
+        '最小值': np.min(kc_stock_ret),
+    }, name=stock_name)
+    statistics_s = statistics_s.apply(format_number)
+    return statistics_s
+
+
+def format_number(value):
+    if isinstance(value, int):
+        return value
+    elif value.is_integer():
+        return int(value)
+    else:
+        return f'{value:.2%}'
+
+
+def plot_hist_performance(kc_stock_ret, port_ret, kc50_ret, stock_name, date):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    fig = plt.figure(figsize=(12, 8))
+    sns.distplot(kc_stock_ret, bins=100, kde=False)
+    plt.title(f'{stock_name}涨跌幅分布')
+    plt.xlabel('涨跌幅')
+    plt.ylabel('计数')
+    plt.axvline(x=port_ret, color='r', linestyle='--', label='踏浪1号')
+    plt.axvline(x=kc50_ret, color='b', linestyle='--', label='科创50指数')
+    plt.axvline(x=np.mean(kc_stock_ret), color='g', linestyle='--', label=f'{stock_name}均值')
+    plt.axvline(x=np.median(kc_stock_ret), color='orange', linestyle='--', label=f'{stock_name}中位数')
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    plt.legend()
+    plt.savefig(f'./data/{stock_name}涨跌幅分布_{date}.png')
+
+
+def hist_performance(kc_stock_ret, port_ret, kc50_ret, stock_name, date):
+    counts, bins = np.histogram(kc_stock_ret.values, bins=100)
+    bins = 0.5 * (bins[1:] + bins[:-1])
+    fig = px.bar(
+        x=bins,
+        y=counts,
+        title=f'{stock_name}涨跌幅分布',
+        labels={'x': '涨跌幅', 'y': '计数'},
+    )
+    fig.update_layout(
+        xaxis_title="涨跌幅",
+        yaxis_title="计数",
+        title_x=0.5,
+        legend_title="参数",
+        font=dict(
+            family="Courier New, monospace",
+            size=12,
+            color="RebeccaPurple",
+        ),
+        width=1200,
+        height=800,
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+    )
+    line_dict = {
+        '踏浪1号涨跌幅': {'value': port_ret, 'color': '#F21526'},
+        '科创50涨跌幅': {'value': kc50_ret, 'color': '#78B4FC'},
+        f'{stock_name}涨跌幅均值': {'value': np.mean(kc_stock_ret), 'color': 'green'},
+        f'{stock_name}涨跌幅中位数': {'value': np.median(kc_stock_ret), 'color': 'orange'},
+    }
+    for label, item in line_dict.items():
+        fig.add_traces(
+            go.Scatter(
+                x=[item['value'], item['value']],
+                y=[0, max(counts) * 1.1],
+                mode='lines',
+                name=label,
+                line=dict(color=item['color'], width=1.5, dash='dash')
+            )
+        )
+    fig.show()
+    with open(f'./data/{stock_name}涨跌幅分布_{date}.html', 'w', encoding='utf-8') as f:
+        write_html(fig, f)
+
+
+def get_data(date=None):
+    date = time.strftime('%Y-%m-%d') if date is None else pd.to_datetime(date).strftime("%Y-%m-%d")
+    ret_dict = {
+        'talang1': get_talang1_ret(date),
+        'kc50': get_kc50_ret(date),
+        'kc stock': get_kc_stock_pct(date),
+    }
+    kc50_stock_list = get_kc50_stock_list(date)
+    ret_dict.update({
+        'kc50 stock': ret_dict['kc stock'][kc50_stock_list],
+    })
+    return ret_dict
+
+
+if __name__ == '__main__':
+    daily_performance_eval(date='20231019')
---
+++ performance_analysis/data_acquisition.py
@@ -0,0 +1,43 @@
+import os
+import time
+
+import pandas as pd
+import rqdatac as rq
+
+
+def get_talang1_ret(date=None):
+    date = pd.to_datetime(date).strftime('%Y%m%d') if date is not None else time.strftime('%Y%m%d')
+    account_df = pd.read_excel(rf'C:\Users\Yz02\Desktop\strategy_update\cnn策略观察_{date}.xlsx', sheet_name='踏浪1号',
+                               index_col=0)
+    account_df.index = account_df.index.astype(str)
+    talang1_ret = account_df.loc[date, '当日收益率']
+    return talang1_ret
+
+
+def get_kc50_stock_list(date=None):
+    date = date if date is not None else time.strftime('%Y-%m-%d')
+    rq.init()
+    return rq.index_components('000688.XSHG', date=date)
+
+def get_kc50_ret(date=None):
+    date = pd.to_datetime(date).strftime('%Y-%m-%d') if date is not None else time.strftime('%Y-%m-%d')
+    rq.init()
+    return rq.get_price_change_rate('000688.XSHG', start_date=date, end_date=date).iloc[0, 0]
+
+def get_kc_stock_pct(date=None):
+    date = pd.to_datetime(date).strftime('%Y-%m-%d') if date is not None else time.strftime('%Y-%m-%d')
+    kc_stock_s = get_kc_stock_info(date)['order_book_id']
+    pct_s = rq.get_price_change_rate(kc_stock_s.tolist(), start_date=date, end_date=date).iloc[0]
+    return pct_s
+
+
+def get_kc_stock_info(date=None):
+    date = date if date is not None else time.strftime('%Y-%m-%d')
+    rq.init()
+    kc_stock_info_df = rq.all_instruments(type='CS', market='cn', date=date).query('board_type=="KSH"')
+    kc_stock_info_df.to_pickle(rf'./data/kc_stock_info_{date}.pkl')
+    return kc_stock_info_df
+
+if __name__ == '__main__':
+
+    print(get_kc50_ret())
