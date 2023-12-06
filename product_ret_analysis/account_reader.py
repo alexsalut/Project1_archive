ed797c3ef5fae8886e6c169463be88a2a9ac206c
Suying Zhou <zhou.sy@yz-fund.com.cn>
2023-12-06 09:54:12 +0800

    盼澜1号， 听涟2号，踏浪1号产品收益率分析，包括股票持有+交易收益分解， 期权持有+交易+ETF基差+跟踪误差分解等

---
+++ product_ret_analysis/account_reader.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/12/5 9:11
+# @Author  : Suying
+# @Site    : 
+# @File    : account_reader.py
+import time
+import pandas as pd
+import numpy as np
+from record.cats_file_reader import CatsFileReader
+from util.file_location import FileLocation
+def get_product_record(product, indicator_name, date=None):
+    record_path = FileLocation.record_path
+    date = time.strftime('%Y%m%d') if date is None else date
+    record_df = pd.read_excel(record_path, sheet_name=product, header=0, index_col=0)
+    record_df.index = record_df.index.astype(str)
+    return float(record_df.loc[date, indicator_name])
+
+
+def get_monitor_data(indicator_name, date=None):
+    date = time.strftime('%Y%m%d') if date is None else date
+    monitor_df = pd.read_excel(rf'{FileLocation.remote_monitor_dir}/monitor_{date}.xlsx', sheet_name=0, header=0, index_col=False)
+    def get_values(df, indicator):
+        loc = np.where(df.values == indicator)
+        return df.iloc[loc[0][0], loc[1][0]+1]
+    return get_values(monitor_df, indicator_name)
+
+
+
+def get_transaction_df(account, type, date=None):
+    date = time.strftime('%Y%m%d') if date is None else date
+    if type != 'Option' and account == '听涟2号':
+        transaction_df = pd.read_csv(
+            rf'{FileLocation.emc_dir}/310310300343_RZRQ_MATCH.{date}.csv',
+            index_col=False,
+            encoding='gbk'
+        ).rename(columns={'证券代码':'代码','信用交易类型': '交易类型'})
+    else:
+        account_code = FileLocation.account_code[account]
+        df = (CatsFileReader(file_dir=FileLocation.cats_dir, account_code=account_code, date=date).get_transaction_df())
+        df = df.rename(columns={'证券代码':'代码','交易': '交易类型','成交量':'成交数量','成交均价':'成交价格','成交额': '成交金额'})
+        if len(df) > 0:
+            df['代码'] = df['代码'].str.split('.', expand=True)[0]
+        if type == 'Option':
+            transaction_df = df.query('代码.str.startswith("100")', engine='python')
+        else:
+            transaction_df = df.query('~代码.str.startswith("100")', engine='python')
+    transaction_df['成交方向'] = transaction_df['交易类型'].map(lambda x: 1 if '买入' in x else -1)
+    transaction_df['成交金额'] = transaction_df['成交金额'].mul(transaction_df['成交方向'])
+    new_transaction_df = transaction_df.groupby('代码')[['成交金额', '成交数量']].sum()
+    return transaction_df, new_transaction_df
+
+
+
+
+def get_position_s(account, type, date=None):
+    date = time.strftime('%Y%m%d') if date is None else date
+    account_code = FileLocation.account_code[account]
+    if type == 'Option':
+        account_code =  FileLocation.option_account_code[account]
+        df = CatsFileReader(file_dir=FileLocation.cats_dir, account_code=account_code, date=date).get_trading_position(type+'Position')
+        df['买卖方向'] = df['持仓方向'].apply(lambda x: -1 if x=='义务仓' else 1)
+        df['持有数量'] = df['买卖方向'].mul(df['可用数量'])
+        df = df.rename(columns={'合约编号':'代码'})
+        position_s = df.groupby(['代码'])['持有数量'].sum()
+    elif account in ['踏浪1号','盼澜1号']:
+        df = CatsFileReader(file_dir=FileLocation.cats_dir, account_code=account_code, date=date).get_trading_position(type+'Position')
+        position_s = df.rename(columns={'当前余额':'持有数量'}).groupby('代码')['持有数量'].sum()
+    elif account == '听涟2号':
+        position_df = pd.read_csv(
+            rf'{FileLocation.emc_dir}/310310300343_RZRQ_POSITION.{date}.csv',
+            encoding='gbk',
+            index_col=False).rename(columns={'证券代码':'代码','持仓数量':'持有数量'})
+        position_s = position_df.groupby('代码')['持有数量'].sum()
+    else:
+        raise KeyError
+    return position_s
+
+
+if __name__ == '__main__':
+    get_transaction_df('盼澜1号', type='Option', date='20231201')
+
+
+
+
---
+++ product_ret_analysis/product_ret_decomposition.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/12/5 9:09
+# @Author  : Suying
+# @Site    : 
+# @File    : product_ret_decomposition.py
+
+import time
+import pandas as pd
+import rqdatac as rq
+from util.send_email import Mail, R
+from util.trading_calendar import TradingCalendar as tc
+from product_ret_analysis.account_reader import get_position_s, get_transaction_df, get_product_record, get_monitor_data
+from util.file_location import FileLocation
+
+
+class ProductRetDecomposition:
+    def __init__(self, date=None):
+        self.date = time.strftime('%Y%m%d') if date is None else date
+        self.last_trading_day = tc().get_n_trading_day(self.date, -1).strftime('%Y%m%d')
+        self.stock_list = ['踏浪1号', '盼澜1号', '听涟2号']
+        self.option_list = ['听涟2号', '盼澜1号']
+        self.stock_fee = {
+            '盼澜1号': {'买入': 0.01154 / 100, '卖出': 0.06154 / 100},
+            '听涟2号': {'担保品买入': 0.01354 / 100, '担保品卖出': 0.06354 / 100, '融资买入': 0.01354 / 100}
+        }
+        self.option_tickers = ['10005541', '10005532', '588000.XSHG'] # d（沽-购 + ETF）/ETF
+
+
+    def gen_email(self):
+        table_1, table_2 = self.gen_table()
+        styled_1, styled_2 = self.format_table(table_1), self.format_table(table_2)
+        content = f"""
+    <table width="800" border="0" cellspacing="0" cellpadding="4">
+    <tr>
+    <td bgcolor="#CECFAD" height="30" style="font-size:21px"><b>踏浪1号|盼澜1号|听涟2号 持仓核对结果</b></td>
+    </tr>
+    <td bgcolor="#EFEBDE" height="100" style="font-size:13px">
+    """
+        content += f"""
+        <p>各产品交易终端收益率分析</p>
+        <center>{styled_1}</center>
+        <p>注：</p>
+        <p>收益率1和2均是通过交易终端导出单计算得到</p>
+        <p>收益率1=净资产的增长率（导出单Fund文件）</p>
+        <p>收益率2=交易收益率（导出单Trade文件）+ 持有收益率（导出单Position文件）</p>
+        """
+
+        content += f"""
+        <center>{styled_2}</center>
+        <p>注：</p>
+        <p>股票权重配置误差=股票持有收益率-monitor组合涨幅</p>
+        <p>期权权重配置误差=期权持有收益率+指数收益率+ETF跟踪误差-期权基差</p>
+        """
+
+        subject = f'[产品收益率分析] {self.date}'
+        Mail().send(subject=subject,
+                    body_content=content,
+                    receivers=R.department['research'])
+
+
+    def format_table(self, df):
+        styled_df = df.style.format({product : '{:.2%}' for product in self.stock_list})
+        styled_df = styled_df.to_html(classes='table', escape=False)
+        styled_df = styled_df.replace('<table',
+                                              '<table style="border-collapse: collapse; border: 1px solid black;"')
+        styled_df = styled_df.replace('<tr', '<tr style="border-bottom: 1px solid black;"')
+        styled_df = styled_df.replace('<th', '<th style="border-right: 1px solid black;"')
+        styled_df = styled_df.replace('<td', '<td style="border-right: 1px solid black;"')
+        styled_df = styled_df.replace('nan%', '-')
+        return styled_df
+
+    def gen_table(self):
+        df = self.gen_df()
+        table_1 = df.loc[['股票收益率1', '股票收益率2', '股票收益误差', '期权收益率1', '期权收益率2', '期权收益误差'],:]
+        table_2_row = [x for x in df.index if x not in table_1.index]
+        table_2 = df.loc[table_2_row, :]
+        return table_1, table_2
+
+    def gen_df(self):
+        data = []
+        for product in self.stock_list:
+            s = pd.Series(self.get_ret_decomposition(product), name=product)
+            data.append(s)
+        df = pd.concat(data, axis=1)
+        return df
+
+    def get_ret_decomposition(self, product):
+        product_dict = {}
+        stock_asset = get_product_record(product, '股票资产总值', self.date)
+
+        product_dict['股票收益率1'] = get_product_record(product, '股票盈亏', self.date) / stock_asset
+        product_dict['股票交易收益率'] = self.get_trade_pl(product, 'Credit') / stock_asset
+        product_dict['股票持有收益率'] = self.get_hold_pl(product, 'Credit') / stock_asset
+        product_dict['股票收益率2'] = product_dict['股票交易收益率'] + product_dict['股票持有收益率']
+        product_dict['股票收益误差'] = product_dict['股票收益率1'] - product_dict['股票收益率2']
+        product_dict['monitor组合涨幅'] = get_monitor_data(product, self.date)
+        product_dict['股票权重配置误差'] = product_dict['股票持有收益率'] - product_dict['monitor组合涨幅']
+
+        if product in self.option_list:
+            product_dict['期权收益率1'] = get_product_record(product, '期权盈亏', self.date) / stock_asset
+            product_dict['期权交易收益率'] = self.get_trade_pl(product, 'Option') / stock_asset
+            product_dict['期权持有收益率'] = self.get_hold_pl(product, 'Option') / stock_asset
+            product_dict['期权收益率2'] = product_dict['期权交易收益率'] + product_dict['期权持有收益率']
+            product_dict['期权收益误差'] = product_dict['期权收益率1'] - product_dict['期权收益率2']
+            product_dict['指数收益率'] = get_monitor_data('000688.SH', self.date)
+            product_dict['期权基差'], etf_ret = self.get_base_diff()
+            product_dict['ETF跟踪误差'] = etf_ret - product_dict['指数收益率']
+            product_dict['期权权重配置误差'] = product_dict['期权持有收益率'] + product_dict['指数收益率'] + product_dict['ETF跟踪误差'] - product_dict['期权基差']
+
+        return product_dict
+
+    def get_trade_fee(self, product, type):
+        sep = '*' * 32
+        print(fr'{sep}Generating {product} {type} Trading fee {sep}')
+        trade_df, _ = get_transaction_df(product, type, self.date)
+
+    def get_trade_pl(self, product, type):
+        sep = '*' * 32
+        print(fr'{sep}Generating {product} {type} Trading P&L {sep}')
+        _, trade_df = get_transaction_df(product, type, self.date)
+        trade_df.index = trade_df.index.astype(str)
+        ticker_list = trade_df.index.astype(str).tolist()
+        if ticker_list:
+            new_trade_df = pd.concat([trade_df, get_t_raw_daily_bar(ticker_list=ticker_list, type=type, date=self.date)], axis=1)
+            trade_pl = new_trade_df.apply(
+                lambda x: x['成交数量'] * x['close'] - x['成交金额'] if x['成交金额'] > 0
+                else -x['成交数量'] * x['close'] - x['成交金额'], axis=1).sum()
+        else:
+            trade_pl = 0
+        print(f'{product} {type} Trading P&L is {trade_pl}')
+        return trade_pl
+
+    def get_hold_pl(self, product, type):
+        sep = '*' * 32
+        print(fr'{sep}Generating {product} {type} Holding P&L {sep}')
+        position_s = get_position_s(account=product, type=type, date=self.last_trading_day)
+        ticker_list = position_s.index.astype(str).tolist()
+
+        last_close = get_t_raw_daily_bar(ticker_list=ticker_list, type=type, date=self.last_trading_day)
+        today_close = get_t_raw_daily_bar(ticker_list=ticker_list, type=type, date=self.date)
+        position_s = get_position_s(account=product, type=type, date=self.last_trading_day)
+        position_s.index = position_s.index.astype(str)
+        hold_pl = ((today_close - last_close).mul(position_s)).sum()
+        print(f'{product} {type} Holding P&L is {hold_pl}')
+        return hold_pl
+
+    def get_base_diff(self):
+        l = self.option_tickers
+        df = get_t_raw_daily_bar(
+                                    ticker_list=l,
+                                    type='Option',
+                                    col=['close', 'prev_close'],
+                                    date=self.date
+        )
+        s = df['close'] - df['prev_close']
+        base_diff = (s[l[0]] - s[l[1]] + s[l[2]])/df.loc[l[2],'prev_close']
+        return base_diff, s[l[2]]/df.loc[l[2], 'prev_close']
+
+
+def get_t_raw_daily_bar(ticker_list, type, col='close', date=None):
+    if type == 'Option':
+        rq.init()
+        raw_daily = rq.get_price(ticker_list, start_date=date, end_date=date).droplevel(1)
+        return raw_daily[col] * 10000
+
+
+    else:
+        date = time.strftime('%Y%m%d') if date is None else pd.to_datetime(date).strftime('%Y%m%d')
+        t_raw_daily_bar_dir = FileLocation.raw_daily_dir
+        file_path = rf'{t_raw_daily_bar_dir}\{date[:4]}\{date[:6]}\raw_daily_{date}.csv'
+        raw_daily_df = pd.read_csv(file_path, index_col=0)
+        raw_daily_df.index = raw_daily_df.index.str.split('.', expand=True).get_level_values(0)
+        return raw_daily_df.loc[ticker_list, col]
+
+
+if __name__ == '__main__':
+    ProductRetDecomposition('20231205').gen_email()
